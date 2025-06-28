aac276265a1a34965e9dcf673cee79a0514c10a0
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-06-28 08:45:10 +0530

    terraform.tfvars

---
+++ for-loop/ec2-user.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "roboshop" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.allow_alls.id ]
+  tags = var.tags
+  
+}
+
+resource "aws_security_group" "allow_alls" {
+    name        = var.sg_name # calling from terraform.tfvars file
+    description = var.sg_description # calling from tf variable.tf file
+    ingress {
+        from_port= var.from_port
+
+        to_port    = var.to_port
+        protocol         = "-1"
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = "-1"
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags  = var.sg_tags
+    
+}
---
+++ for-loop/variable.tf
@@ -0,0 +1,39 @@
+variable "ami_id" {
+    type = string
+  default ="ami-09c813fb71547fc4f"
+  description = "AMI"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "tags" {
+  type = map(string) 
+ default= {
+    Name = "Hello-world"
+    Purpose = "variable_demo"
+  }
+}
+variable "sg_name"{
+    default=" var-flies-allow_alls"
+}
+variable "sg_description"{
+    default="allow all traffic"
+}
+variable "from_port"{
+    type= number
+    default = 0
+}
+variable "to_port"{
+    type= number
+    default = 0
+}
+variable "cidr_blocks"{
+
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+variable "sg_tags" {
+    default = {
+        Name = "allow-alls"
+    }
+}
